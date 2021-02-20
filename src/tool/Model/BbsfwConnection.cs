7f0a22ece90395b50cac8d44fa5a5c273b640b5e
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2021-02-20 22:03:09 +0100

    list physical com ports in config tool

--- src/tool/Model/BbsfwConnection.cs
+++ src/tool/Model/BbsfwConnection.cs
@@ -1,16 +1,77 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
 using System.Text;
 
 namespace BBSFW.Model
 {
-	public class BbsfwConnection
+	public class ComPort
 	{
 
+		public string Name { get; private set; }
+
+		public string Description { get; private set; }
+
+
+		public ComPort(string name, string description)
+		{
+			Name = name;
+			Description = description;
+		}
+	}
+
+
+	public class BbsfwConnection
+	{
+		private SerialPort _port = null;
+
+		public bool IsConnected { get; private set; }
 
 
 
+		public void Open(ComPort port)
+		{
+			_port = new SerialPort(port.Name, 1200);
+			_port.Open();
 
+			// :TODO: start sending hello until response
+
+		}
+
+		public void Close()
+		{
+			if (_port != null)
+			{
+				_port.Close();
+				_port = null;
+			}
+		}
+
+
+
+		public static List<ComPort> GetComPorts()
+		{
+			var result = new List<ComPort>();
+
+			using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+			{
+				var portNames = SerialPort.GetPortNames();
+				var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+
+				foreach (var name in portNames)
+				{
+					var port = ports.FirstOrDefault(p => String.Equals(p["DeviceID"].ToString(), name));
+					if (port != null)
+					{
+						result.Add(new ComPort(name, port["Caption"].ToString()));
+					}
+				}
+			}
+
+			return result;
+		}
 
 	}
 }
--- src/tool/ViewModel/ConnectionViewModel.cs
+++ src/tool/ViewModel/ConnectionViewModel.cs
@@ -1,3 +1,4 @@
+using BBSFW.Model;
 using BBSFW.ViewModel.Base;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,42 @@ namespace BBSFW.ViewModel
 {
 	public class ConnectionViewModel : ObservableObject
 	{
+
+		private List<ComPort> _comPorts;
+		public List<ComPort> ComPorts
+		{
+			get { return _comPorts; }
+			set
+			{
+				if (_comPorts != value)
+				{
+					_comPorts = value;
+					OnPropertyChanged(nameof(ComPorts));
+				}
+			}
+		}
+
+		private ComPort _selectedComPort;
+		public ComPort SelectedComPort
+		{
+			get { return _selectedComPort; }
+			set
+			{
+				if (_selectedComPort != value)
+				{
+					_selectedComPort = value;
+					OnPropertyChanged(nameof(SelectedComPort));
+				}
+			}
+		}
+
+
+
+		public ConnectionViewModel()
+		{
+			ComPorts = BbsfwConnection.GetComPorts();
+		}
+
 
 
 
